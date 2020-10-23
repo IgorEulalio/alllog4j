8431b2c9ad7e619ecc28a75164929948757c016e
IgorEulalio <igoreulalio.ie@gmail.com>
2020-10-23 16:57:35 -0300

    iniciando projeto

---
+++ src/main/java/com/fulllog/alllog4j/Alllog4jApplication.java
@@ -0,0 +1,13 @@
+package com.fulllog.alllog4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alllog4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Alllog4jApplication.class, args);
+	}
+
+}
